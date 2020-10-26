c494e1cf349a46d9114aa6ec0348aec488aa980f
Joram <jing111597@gmail.com>
2020-10-26 21:56:59 +0800

    complete ejs part

--- app.js
+++ app.js
@@ -2,11 +2,40 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+var item = ["wash cloth"];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 
 
 
-app.get("/", function(req,res){
-   res.send("hit port 3000");
+
+app.get("/", function (req, res) {
+   
+
+   var options = {
+      day: "numeric",
+      month: "long",
+      year: "numeric"
+   }
+   var today = new Date();
+   var day = today.toLocaleDateString("en-US", options);
+  
+
+  
+
+   res.render("list", {
+      kindofday: day,
+      newitem: item
+   });
+});
+
+app.post("/", function (req,res) {
+   item.push(req.body.newItem);
+   
+   res.redirect("/")
+   
 });
 
 app.listen(3000, function(req,res){
